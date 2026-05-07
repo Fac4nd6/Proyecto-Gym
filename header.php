177be092c2a9fc434c614d6385efbe4ba1610e5c
Facundo Leites <fac4nd6@gmail.com>
2026-05-06 21:37:10 -0300

    Primer commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GymLife</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+  <header>
+    <nav class="nav">
+
+        <div class="nav-left">
+            <a href="#">Home</a>
+        </div>
+
+        <div class="nav-center">
+            <p>Gym<br>For<br>Life</p>
+        </div>
+
+        <div class="nav-right">
+            <a href="#">Login</a>
+            <a href="#">Events</a>
+        </div>
+
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require "header.php";
+?>
+<main>
+
+    <!-- HERO -->
+    <section class="hero">
+
+        <div class="info-card">
+
+            <div>
+                <p>Location:</p>
+                <span>Green Bay Trail, Glencoe, IL</span>
+            </div>
+
+            <div>
+                <p>Open</p>
+                <span>Monday to Saturday</span>
+            </div>
+
+            <div>
+                <p>Time</p>
+                <span>6AM - NOON</span>
+            </div>
+
+        </div>
+
+        <button class="btn-register">REGISTER</button>
+
+    </section>
+
+    <section class="hero-2">
+
+    </section>
+
+    <section class="split">
+
+        <div class="hero-left">
+            <div class="overlay">
+                <h2>500+<br>Members</h2>
+                <p>By North Shore residents</p>
+                <button>ABOUT US</button>
+            </div>
+        </div>
+
+        <div class="hero-right">
+            <p>Don't Skip a Rep</p>
+            <p>Get the latest updates and start your fitness journey today.</p>
+        </div>
+
+    </section>
+
+    <section class="hero4">
+
+
+    </section>
+</main>
+
+<!-- FOOTER -->
+<footer>
+
+</footer>
+
+</body>
+
+</html>
