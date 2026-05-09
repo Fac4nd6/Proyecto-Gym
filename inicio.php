7290b821b6680a710880309fadb07fdb93140ce2
Facundo Leites <fac4nd6@gmail.com>
2026-05-09 17:05:00 -0300

    Se añadio al comienzo del archivo en php la verificacion de si exsiste la SESSION username o no en caso de que no lo redirigue al Login para iniciar Sesion

--- inicio.php
+++ inicio.php
@@ -1,11 +1,128 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
 require "header.php";
 ?>
 
+
 <main>
 
+    <!-- HERO -->
+    <section class="Inicio-hero negro">
+
+        <div class="hero2-content">
+            <h2>Welcome</h2>   
+            <?php echo "<p>" . $_SESSION['username'] . "</p>"; ?>  
+        </div>
+
+    </section>
+
+
+
+    <section class="Inicio-hero chest">
+
+    <div class="hero2-content">
+        
+        <h2>
+            Chest Day, Best Day.
+        </h2>
+
+        <p>
+            Challenge yourself with our Chest Day routine. A complete workout designed to build strength, improve your posture and push your limits one rep at a time.
+        </p>
+
+        <button>VIEW EXERCISES</button>
+
+    </div>
+
+    </section>
+
+     <section class="Inicio-hero leg">
+
     
+
+    <div class="hero2-content">
+        
+        <h2>
+            Leg Day, Strongest Day.
+        </h2>
+
+        <p>
+            Challenge yourself with our Leg Day routine. A complete workout designed to build powerful legs, improve your balance and push your endurance to the next level.
+        </p>
+
+        <button>VIEW EXERCISES</button>
+
+    </div>
+
+    </section>
+
+     <section class="Inicio-hero arm">
+
     
+
+    <div class="hero2-content">
+        
+        <h2>
+            Arm Day, Pump Day.
+        </h2>
+
+        <p>
+            Challenge yourself with our Arm Day routine. A complete workout designed to build stronger arms, increase your muscle definition and push your strength to the limit.
+        </p>
+
+        <button>VIEW EXERCISES</button>
+
+    </div>
+
+    </section>
+
+     <section class="Inicio-hero shoulder">
+
+    
+
+    <div class="hero2-content">
+        
+        <h2>
+            Shoulder Day, Power Day.
+        </h2>
+
+        <p>
+            Challenge yourself with our Shoulder Day routine. A complete workout designed to build powerful shoulders, improve your stability and push your strength to new heights.
+        </p>
+
+        <button>VIEW EXERCISES</button>
+
+    </div>
+
+    </section>
+
+     <section class="Inicio-hero back">
+
+    
+
+    <div class="hero2-content">
+        
+        <h2>
+            Back Day, Wing Day.
+        </h2>
+
+        <p>
+          Challenge yourself with our Back Day routine. A complete workout designed to strengthen your back, improve your posture and build the foundation of a stronger body.
+        </p>
+
+        <button>VIEW EXCERCISE</button>
+
+    </div>
+
+    </section>
+
+
 </main>
 
 <?php
